ccd5d914b18907ffb8e9f0d1ec4bb315c773e1e6
Ayomide-Oyeneye <nickbelson11@gmail.com>
2024-08-28 05:39:25 +0100

    portfolio

--- src/code_components/errorpage.js
+++ src/code_components/errorpage.js
@@ -1,17 +1,23 @@
-import '../App.css'
+import '../App.css';
+import './Error.css'
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Animation from "../images/Animation.gif" ;
+import Animation from "../images/Animation.gif";
+
 function Errorpage() {
     return(
         <>
-<section classNmae="error-page">
-    <div>
-        <p>Error 404</p>
-        <img src={Animation} alt="logo" className='four-img' />
-        <Link to="/">bbbb</Link>
-    </div>
-</section>
+            <section className="error-page">
+                <div className="error-container">
+                   
+                    <h1>Error 404</h1>
+                    <p>Oops! The page you're looking for doesn't exist.</p>
+                    <Link to="/" className="error-link">
+                        <button className="back-button">Back to Home Page</button>
+                    </Link>
+                </div>
+            </section>
         </>
     );
-};
+}
+
 export default Errorpage;
